7671f2277e15fbca72382e654514331c163b4270
kaede-creator <akeed-000@outlook.jp>
2023-09-20 13:20:46 +0900

    Vue追加

---
+++ section2/dist/object.js
@@ -0,0 +1,7 @@
+const objectTest = {
+    name: '堂安',
+    age: 25
+};
+console.log(objectTest.name);
+// オブジェクトも基本は一緒。変数を定義。「:」で型を定義。さらに値にも型を付与する
+//# sourceMappingURL=object.js.map
---
+++ section2/src/object.ts
@@ -0,0 +1,11 @@
+const objectTest:{
+    name:string,
+    age:number
+} = {
+    name:'堂安',
+    age:25
+}
+
+console.log(objectTest.name)
+
+// オブジェクトも基本は一緒。変数を定義。「:」で型を定義。さらに値にも型を付与する
